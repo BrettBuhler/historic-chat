e8cb57c846587f5f0a9eecabf7b5022aeb8fe2b9
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2023-07-12 15:47:10 -0600

    Implemented React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  )
+    return (
+        <div>
+            <Routes>
+                <Route path='/' element = {<h1>HELLO</h1>}/>
+                <Route path='/chat' element = {<h1>HELLO CHAT</h1>}/>
+            </Routes>
+        </div>
+    )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 )
 
 root.render(
-  <App/>
+    <Router>
+        <App/>
+    </Router>
 )
 
